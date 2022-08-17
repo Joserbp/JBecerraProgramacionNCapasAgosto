472c95e36d99a0122c59025783f506708c848d59
Joserbp <josera.jack@gmail.com>
2022-08-17 17:50:37 -0500

    Método add SqlClient

---
+++ BL/Alumno.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BL
+{
+    public class Alumno
+    { 
+        public static ML.Result Add(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result(); //Intancia
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.Get()))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = "INSERT INTO [Alumno]([Nombre],[ApellidoPaterno],[ApellidoMaterno],[FechaNacimiento],[Sexo])VALUES(@Nombre, @ApellidoPaterno, @ApellidoMaterno, @FechaNacimiento, @Sexo)";
+                        cmd.Connection = context;
+
+                        SqlParameter[] collection = new SqlParameter[5];
+                        collection[0] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        collection[0].Value = alumno.Nombre;
+                        collection[1] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        collection[1].Value = alumno.ApellidoPaterno;
+                        collection[2] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        collection[2].Value = alumno.ApellidoMaterno;
+                        collection[3] = new SqlParameter("FechaNacimiento", SqlDbType.Date);
+                        collection[3].Value = alumno.FechaNacimiento;
+                        collection[4] = new SqlParameter("Sexo", SqlDbType.Char);
+                        collection[4].Value = alumno.Sexo;
+
+                        cmd.Parameters.AddRange(collection);
+
+                        cmd.Connection.Open();
+
+                        int RowAffect = cmd.ExecuteNonQuery();
+                        if (RowAffect > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct=false;
+                            result.ErrorMessage = "Ocurrio un error al insertar el Alumno";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Alumno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno(); //Instancia de alumno acceder a las propiedades
+
+            Console.WriteLine("Ingese el Nombre");
+            alumno.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingese el Apellido Paterno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+            Console.WriteLine("Ingese el Apellido Materno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+            Console.WriteLine("Ingese el FechaNacimiento");
+            alumno.FechaNacimiento = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingese el sexo");
+            alumno.Sexo = Console.ReadLine();
+
+            ML.Result result = BL.Alumno.Add(alumno);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("Alumno agregado exitosamente");
+            }
+            else {
+                Console.WriteLine("Alumno no se agrego exitosamente" + result.ErrorMessage);
+            }
+        
+        }
+    }
+}
